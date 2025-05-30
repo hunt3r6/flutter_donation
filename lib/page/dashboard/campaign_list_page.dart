58732fe1cd494e3a9d5988ab41672a5ef14bf492
hunt3r6 <faisalmahadi126@gmail.com>
2025-05-30 07:14:39 +0700

    add action to detail

--- lib/page/dashboard/campaign_list_page.dart
+++ lib/page/dashboard/campaign_list_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_donation/core/util/currency_helper.dart';
 import 'package:flutter_donation/core/widget/custom_app_bar.dart';
 import 'package:flutter_donation/core/widget/fund_progress_bar.dart';
 import 'package:flutter_donation/resource/model/campaign_model.dart';
+import 'package:go_router/go_router.dart';
 
 class CampaignListPage extends StatefulWidget {
   const CampaignListPage({super.key});
@@ -58,60 +59,67 @@ class _CampaignListPageState extends State<CampaignListPage> {
       elevation: 0.2,
       color: Colors.white,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              height: 100,
-              width: 150,
-              child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-                child: CachedNetworkImage(
-                  imageUrl: campaign.image ?? '',
-                  placeholder:
-                      (context, url) =>
-                          Center(child: CircularProgressIndicator()),
-                  errorWidget: (context, url, error) => Icon(Icons.error),
-                  fit: BoxFit.cover,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(15),
+        onTap: () => context.push('/detail_campaign/${campaign.slug}'),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                height: 100,
+                width: 150,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                  child: CachedNetworkImage(
+                    imageUrl: campaign.image ?? '',
+                    placeholder:
+                        (context, url) =>
+                            Center(child: CircularProgressIndicator()),
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 8, right: 8, bottom: 8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 8,
-                children: [
-                  Text(
-                    campaign.title ?? '',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8, right: 8, bottom: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 8,
+                  children: [
+                    Text(
+                      campaign.title ?? '',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
 
-                  FundProgressBar(
-                    collectedAmount: double.parse(
-                      campaign.sumDonation?[0].total ?? '0',
+                    FundProgressBar(
+                      collectedAmount: double.parse(
+                        campaign.sumDonation?[0].total ?? '0',
+                      ),
+                      maxAmount: campaign.targetDonation?.toDouble() ?? 0.0,
                     ),
-                    maxAmount: campaign.targetDonation?.toDouble() ?? 0.0,
-                  ),
-                  Text(
-                    CurrencyHelper.formatRupiah(
-                      double.parse(campaign.sumDonation?[0].total ?? '0'),
+                    Text(
+                      CurrencyHelper.formatRupiah(
+                        double.parse(campaign.sumDonation?[0].total ?? '0'),
+                      ),
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
                     ),
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue,
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
